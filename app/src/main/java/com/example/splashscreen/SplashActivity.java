ef9b632ee9ca3024d5b7b6cccd07863cf5e8eb06
s2020120 <mattmillan064@gmail.com>
2022-10-25 21:36:51 +0800

    splash

---
+++ app/src/main/java/com/example/splashscreen/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.splashscreen;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.splash);
+
+        super.onCreate(savedInstanceState);
+        MediaPlayer appIntro;
+        appIntro = MediaPlayer.create(SplashActivity.this,R.raw.app_intro);
+        appIntro.start();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                appIntro.stop();
+                finish();
+            }
+        }, 5000);
+    }
+}
